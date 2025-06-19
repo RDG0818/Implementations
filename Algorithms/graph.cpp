903805b1967a081f6fdac1160c14b7ca79a86c87
Ryan Goodwin <ryangoodwin0818@gmail.com>
2025-06-19 18:39:33 -0500

    finished updated dfs algorithms

--- Algorithms/graph.cpp
+++ Algorithms/graph.cpp
@@ -26,7 +26,7 @@ using namespace std;
 
 // DFS Algorithms
 
-void dfs_path_finding(vector<vector<int>>& graph, int starting_node, int target_node) {
+void dfs_path_finding(vector<vector<int>>& graph, int starting_node, int target_node) { 
     stack<int> st;
     st.push(starting_node);
     vector<bool> visited(graph.size(), false);
@@ -59,29 +59,28 @@ void dfs_path_finding(vector<vector<int>>& graph, int starting_node, int target_
     cout << "No Path Exists";
 }
 
-void undirected_cycle_check(vector<vector<int>>& graph) {
-    stack<int> st;
+void undirected_cycle_check(vector<vector<int>>& graph) { // Assumes graph is undirected
     vector<int> visited(graph.size(), false);
     vector<int> prev(graph.size(), -1);
     bool found_cycle = false;
     int components = 0;
     for (int starting_node = 0; starting_node < graph.size(); starting_node++) {
-        if (!visited[starting_node]) {
-            components++;
-            st.push(starting_node);
-            visited[starting_node] = true;
-            while(!st.empty()) {
-                int curr_node = st.top();
-                st.pop();
-                for (int neighbor : graph[curr_node]) {
-                    if (!visited[neighbor]) {
-                        st.push(neighbor);
-                        visited[neighbor] = true;
-                        prev[neighbor] = curr_node;
-                    }
-                    else if (visited[neighbor] && neighbor != prev[curr_node]) {
-                        found_cycle = true; // Can break here if you do not need to find components
-                    }
+        if (visited[starting_node]) continue;
+        components++;
+        stack<int> st;
+        st.push(starting_node);
+        visited[starting_node] = true;
+        while(!st.empty()) {
+            int curr_node = st.top();
+            st.pop();
+            for (int neighbor : graph[curr_node]) {
+                if (!visited[neighbor]) {
+                    st.push(neighbor);
+                    visited[neighbor] = true;
+                    prev[neighbor] = curr_node;
+                }
+                else if (visited[neighbor] && neighbor != prev[curr_node]) {
+                    found_cycle = true; // Can break here if you do not need to find components
                 }
             }
         }
@@ -92,32 +91,31 @@ void undirected_cycle_check(vector<vector<int>>& graph) {
     cout << "Total Components: " << components << endl;
 }
 
-void directed_cycle_check(vector<vector<int>>& graph) {
-    stack<pair<int, int>> st;
+void directed_cycle_check(vector<vector<int>>& graph) { // Assumes graph is directed
     vector<int> visit_state(graph.size(), 0);
     bool found_cycle = false;
     for (int starting_node = 0; starting_node < graph.size() && !found_cycle; starting_node++) {
-        if (visit_state[starting_node] == 0 && !found_cycle) {
-            st.push(pair(starting_node, 0));
-            visit_state[starting_node] = 1;
-            while (!st.empty()) {
-                auto& [curr_node, idx] = st.top();
+        if (visit_state[starting_node] != 0) continue; 
+        stack<pair<int, int>> st;
+        st.push(pair(starting_node, 0));
+        visit_state[starting_node] = 1;
+        while (!st.empty()) {
+            auto& [curr_node, idx] = st.top();
 
-                if (idx < (int)graph[curr_node].size()) {
-                    int neighbor = graph[curr_node][idx++];
-                    if (visit_state[neighbor] == 0) {
-                        st.push(pair(neighbor, 0));
-                        visit_state[neighbor] = 1;
-                    }
-                    else if (visit_state[neighbor] == 1) { // back edge detected
-                        found_cycle = true;
-                        break;
-                    } 
+            if (idx < (int)graph[curr_node].size()) {
+                int neighbor = graph[curr_node][idx++];
+                if (visit_state[neighbor] == 0) {
+                    st.push(pair(neighbor, 0));
+                    visit_state[neighbor] = 1;
                 }
-                else {
-                    visit_state[curr_node] = 2;
-                    st.pop();
-                }
+                else if (visit_state[neighbor] == 1) { // back edge detected
+                    found_cycle = true;
+                    break;
+                } 
+            }
+            else {
+                visit_state[curr_node] = 2;
+                st.pop();
             }
         }
     }
@@ -125,26 +123,25 @@ void directed_cycle_check(vector<vector<int>>& graph) {
     else cout << "Found Cycle" << endl;
 }
 
-void bipartite_check(vector<vector<int>>& graph) {
-    stack<int> st;
+void bipartite_check(vector<vector<int>>& graph) { // Assumes graph is undirected
     vector<int> color(graph.size(), -1);
     bool bipartite = true;
     for (int starting_node = 0; starting_node < graph.size(); starting_node++) {
-        if (color[starting_node] == -1) {
-            st.push(starting_node);
-            color[starting_node] = 0;
-            while (!st.empty() && bipartite) {
-                int curr_node = st.top();
-                st.pop();
-                for (int neighbor : graph[curr_node]) {
-                    if (color[neighbor] == -1) {
-                        st.push(neighbor);
-                        color[neighbor] = 1 - color[curr_node];
-                    }
-                    else if (color[neighbor] == color[curr_node]) {
-                        bipartite = false;
-                        break;
-                    }
+        if (color[starting_node] != -1) continue;
+        stack<int> st;
+        st.push(starting_node);
+        color[starting_node] = 0;
+        while (!st.empty() && bipartite) {
+            int curr_node = st.top();
+            st.pop();
+            for (int neighbor : graph[curr_node]) {
+                if (color[neighbor] == -1) {
+                    st.push(neighbor);
+                    color[neighbor] = 1 - color[curr_node];
+                }
+                else if (color[neighbor] == color[curr_node]) {
+                    bipartite = false;
+                    break;
                 }
             }
         }
@@ -155,27 +152,26 @@ void bipartite_check(vector<vector<int>>& graph) {
 }
 
 void topological_sort(vector<vector<int>>& graph) { // Assumes graph is directed and acyclic
-    stack<pair<int, int>> st;
     vector<int> visit_state(graph.size(), 0);
     vector<int> sort_order;
     for (int starting_node = 0; starting_node < graph.size(); starting_node++) {
-        if (visit_state[starting_node] == 0) {
-            st.push(pair(starting_node, 0));
-            visit_state[starting_node] = 1;
-            while (!st.empty()) {
-                auto& [curr_node, idx] = st.top();
-                if (idx < (int)graph[curr_node].size()) {
-                    int neighbor = graph[curr_node][idx++];
-                    if (visit_state[neighbor] == 0) {
-                        st.push(pair(neighbor, 0));
-                        visit_state[neighbor] = 1;
-                    }
+        if (visit_state[starting_node] != 0) continue;
+        stack<pair<int, int>> st;
+        st.push(pair(starting_node, 0));
+        visit_state[starting_node] = 1;
+        while (!st.empty()) {
+            auto& [curr_node, idx] = st.top();
+            if (idx < (int)graph[curr_node].size()) {
+                int neighbor = graph[curr_node][idx++];
+                if (visit_state[neighbor] == 0) {
+                    st.push(pair(neighbor, 0));
+                    visit_state[neighbor] = 1;
                 }
-                else {
-                    st.pop();
-                    visit_state[curr_node] = 2;
-                    sort_order.push_back(curr_node);
-                }
+            }
+            else {
+                st.pop();
+                visit_state[curr_node] = 2;
+                sort_order.push_back(curr_node);
             }
         }
     }
@@ -187,8 +183,65 @@ void topological_sort(vector<vector<int>>& graph) { // Assumes graph is directed
 
 }
 
-void kosaraju(vector<vector<int>>& graph) {
+void kosaraju(vector<vector<int>>& graph) { // Assumes graph is directed
+    vector<bool> visit_state(graph.size(), false);
+    vector<int> post_order;
 
+    for (int starting_node = 0; starting_node < graph.size(); starting_node++) {
+        if (visit_state[starting_node] != 0) continue;
+        stack<pair<int, int>> st;
+        st.push(pair(starting_node, 0));
+        visit_state[starting_node] = true;
+        while (!st.empty()) {
+            auto&[curr_node, idx] = st.top();
+
+            if (idx < (int)graph[curr_node].size()) {
+                int neighbor = graph[curr_node][idx++];
+                if (visit_state[neighbor] == 0) {
+                    st.push(pair(neighbor, 0));
+                    visit_state[neighbor] = true;
+                }
+            }
+            else {
+                post_order.push_back(curr_node);
+                st.pop();
+            }
+        }
+    }
+
+    reverse(post_order.begin(), post_order.end());
+    vector<int> component_id(graph.size(), -1);
+    int num_components = 0;
+
+    vector<vector<int>> reversed_graph(graph.size());
+    for (int i = 0; i < graph.size(); i++) {
+        for (int j = 0; j < graph[i].size(); j++) {
+            reversed_graph[graph[i][j]].push_back(i);
+        }
+    }
+
+    for (int starting_node : post_order) {
+        if (component_id[starting_node] != -1) continue;
+        stack<pair<int, int>> st;
+        component_id[starting_node] = num_components++;
+        st.push(pair(starting_node, 0));
+        while (!st.empty()) {
+            auto&[curr_node, idx] = st.top();
+            if (idx < (int)reversed_graph[curr_node].size()) {
+               int neighbor = reversed_graph[curr_node][idx++]; 
+               if (component_id[neighbor] == -1) {
+                   st.push(pair(neighbor, 0));
+                   component_id[neighbor] = component_id[curr_node];
+               }
+            }
+            else {
+                st.pop();
+            }
+        }
+    }
+    cout << "Number of SCC: " << num_components << endl;
+    cout << "Component IDs" << endl; 
+    for (int i = 0; i < component_id.size(); i++) cout << "Node: " << i << " | ID: " << component_id[i] << endl;
 }
 
 void bfs() {
@@ -231,6 +284,6 @@ for (int i = 0; i < M; i++) {
 // undirected_cycle_check(unweighted_graph);
 // bipartite_check(unweighted_graph);
 directed_cycle_check(graph);
-topological_sort(graph);
+kosaraju(graph);
 return 0;
 }
